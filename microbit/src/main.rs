72f672725f743de27cab87f2fd08aac8ea0247f2
Jiping Liu <jipings@outlook.com>
2023-12-01 22:28:05 +0800

    build

--- microbit/src/main.rs
+++ microbit/src/main.rs
@@ -1,19 +1,34 @@
 
 #![no_std]
 #![no_main]
+#![deny(unsafe_code)]
 
-use panic_rtt_target as _;
-use rtt_target::{rtt_init_print, rprintln};
+// use panic_rtt_target as _;
+
+// use rtt_target::{rtt_init_print, rprintln};
 
 use cortex_m_rt::entry;
+use panic_halt as _;
+use microbit as _;
 
 #[entry]
 fn main () -> ! {
-    rtt_init_print!();
-    rprintln!("Hello World!");
+    let _y;
+    let x = 42;
+    _y = x;
+
     loop {}
 }
 
 
-// $ rustup target add thumbv7em-none-eabihf
-// $ cargo embed --target thumbv7em-none-eabihf
+// build 
+// $ cargo build --features v2 --target thumbv7em-none-eabihf
+// 
+// verify
+// $ cargo readobj --features v2 --target thumbv7em-none-eabihf --bin microbit-demo -- --file-headers
+//
+// write it to flash
+// $ cargo embed --features v2 --target thumbv7em-none-eabihf
+// 
+// debugging
+// 
