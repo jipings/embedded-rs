3ebaf9fc69a070966f62fd171b302e53a174e2f2
Jiping Liu <jiping.liu@compass.com>
2023-12-02 00:02:29 +0800

    LED display

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## References
 https://github.com/rust-embedded/discovery/
+
 https://droogmic.github.io/microrust/index.html
+
+https://github.com/microbit-foundation/microbit-v2-hardware/tree/main
--- microbit/src/main.rs
+++ microbit/src/main.rs
@@ -3,21 +3,31 @@
 #![no_main]
 #![deny(unsafe_code)]
 
-// use panic_rtt_target as _;
-
-// use rtt_target::{rtt_init_print, rprintln};
-
 use cortex_m_rt::entry;
+use rtt_target::{rtt_init_print, rprintln};
 use panic_halt as _;
-use microbit as _;
+use microbit::board::Board;
+use microbit::hal::timer::Timer;
+use microbit::hal::prelude::*;
 
 #[entry]
 fn main () -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
 
-    loop {}
+    let mut timer = Timer::new(board.TIMER0);
+    
+    board.display_pins.col1.set_low().unwrap();
+    let mut row1 = board.display_pins.row1;
+
+    loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
+        timer.delay_ms(1_000_u16);
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1_000_u16);
+    }
 }
 
 
